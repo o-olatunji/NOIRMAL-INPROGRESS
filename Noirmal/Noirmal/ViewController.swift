d23edb9bb6bd9298834dccb357d7dea9c3a18ccd
Olamide Olatunji <olatunji14@gmail.com>
2016-10-10 10:31:57 -0700

    commit

---
+++ Noirmal/Noirmal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noirmal
+//
+//  Created by Olamide Olatunji on 10/7/16.
+//  Copyright © 2016 Olamide Olatunji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
