d98eab33524f41abab439578e34b8c9a9ec7c63b
Olamide Olatunji <olatunji14@gmail.com>
2016-11-02 20:11:28 -0700

    commit
    add images

---
+++ Noirmal/Noirmal/Hue.swift
@@ -0,0 +1,29 @@
+//
+//  Hue.swift
+//  Noirmal
+//
+//  Created by Olamide Olatunji on 11/2/16.
+//  Copyright © 2016 Olamide Olatunji. All rights reserved.
+//
+
+import UIKit
+
+class Hue {
+    
+    let name: String
+    let female: UIImage
+    let description: String
+    let actresses: String
+    let hue: UIImage
+    
+    init(name: String, female: UIImage, description: String, actresses: String, hue: UIImage) {
+    
+    self.name = name
+    self.female = female
+    self.description = description
+    self.actresses = actresses
+    self.hue = hue
+    
+}
+    
+}
---
+++ Noirmal/Noirmal/HueTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HueTableViewController.swift
+//  Noirmal
+//
+//  Created by Olamide Olatunji on 11/2/16.
+//  Copyright © 2016 Olamide Olatunji. All rights reserved.
+//
+
+import UIKit
+
+class HueTableViewController: UITableViewController {
+    
+    var hues: [Hue] = [
+    
+    Hue(name: "Tofee", female: #imageLiteral(resourceName: "PaulaPatton"), description: ", actresses: <#T##String#>, hue: <#T##UIImage#>)
+        
+        
+    
+    
+    ]
+    
+}
