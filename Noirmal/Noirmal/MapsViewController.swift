92526033df6c31c84fe9b87de656918eafc7e7ba
Olamide Olatunji <olatunji14@gmail.com>
2016-11-03 11:29:29 -0700

    commit
    building in the search bar

--- Noirmal/Noirmal/MapsViewController.swift
+++ Noirmal/Noirmal/MapsViewController.swift
@@ -10,7 +10,57 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapsViewController: UIViewController {
+class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var resultsSearchController: UISearchController? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //searchbar code
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationTableViewController
+        resultsSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultsSearchController?.searchResultsUpdater = LocationTableViewController
+        let searchBar = resultsSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for Stores"
+        navigationItem.titleView = resultsSearchController?.searchBar
+        resultsSearchController?.hidesNavigationBarDuringPresentation = false
+        resultsSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        // connecting search page table to this map view when searching 
+        locationSearchTable.mapView = mapView
+        
+        
+        //corelocation code
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    //MARK: Location Delegate Methods
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    
+            self.mapView.setRegion(region, animated: true)
+            self.locationManager.stopUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Errors: " + error.localizedDescription)
+    }
     
     
     
